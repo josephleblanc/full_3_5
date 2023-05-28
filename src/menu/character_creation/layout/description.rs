10fdc47ef54deafbc21290b9283dc668d8d34176
Joseph LeBlanc <joseph.t.leblanc@protonmail.com>
2023-05-28 10:30:10 -0700

    Add comments

--- src/menu/character_creation/layout/description.rs
+++ src/menu/character_creation/layout/description.rs
@@ -10,6 +10,7 @@ use bevy::prelude::*;
 
 #[derive(Resource, Clone)]
 pub struct CentralListBundles {
+    // A bundle of bundles used to spawn a list with a unified style.
     list_node: NodeBundle,
     list_item_title: TextBundle,
     list_row_node: NodeBundle,
@@ -23,6 +24,8 @@ pub struct CentralListBundles {
 
 impl CentralListBundles {
     pub fn init(asset_server: Res<AssetServer>, mut commands: Commands) {
+        // A function that may be turned into a system to initialize the resource before
+        // it is referenced by systems which utilize the CentralListBundles.
         let shared_font = asset_server.load(PATH_SIMPLE_FONT);
         let list_node = NodeBundle {
             style: LIST_PARENT_NODE_STYLE,
@@ -150,9 +153,6 @@ impl AsVec for PlayableRace {
     }
 }
 
-use bevy::asset::HandleId;
-use std::iter::Filter;
-
 pub trait HasDescr {
     fn description(&self) -> &String;
 }
@@ -188,13 +188,45 @@ pub fn build_description_list<T, U, V>(
     // This is the identifying enum
     V: Component + AsVec + Eq + PartialEq + std::fmt::Display,
 {
+    // This is a generic function to build a description list in the style of the character
+    // creation menu.
+    //
+    // It takes three generics:
+    //   T: The custom asset, which must have its CustomAssetLoadState<T> initialized, and
+    //       have been loaded with a custom plugin, e.g. app.add_plugin(MyRaceAssetPlugin)
+    //       The custom asset should have some kind of identifying enum, for example with
+    //       race descriptions the enum is PlayableRace.
+    //   U: The label added as a component to the spawned items. This should be a unit struct
+    //       that is unique to that list, so the list may be displayed or hidden by assigning
+    //       the display.style to Display::Flex or Display::None.
+    //   V: The identifying enum noted in T above. This should be an enum without variants
+    //       that is used to associate the text description in the custom asset with a quality
+    //       of the object being described, e.g. PlayableRace, ClassName, etc.
+    //
+    // The function also takes a list_resource parameter, which is used as a template for the
+    // text, node, or button bundles to allow for a more unified presentation of multiple lists
+    // spawned by this function.
+    //
+    // Finally, the function takes a query_parent parameter, which is used to identify the
+    // parent node to which the spawned list will belong as a child. This parent must share
+    // the same label as the list being used. It is not necessary for the parent to only have
+    // one set of children spawned by this function, as items of the list may be shown/hidden,
+    // but it would be best to have a child node in the target area which is the parent of nodes
+    // spawned with this function, so all nodes spawned with this function may be hidden at the
+    // same time without affecting other children in the target area.
+    //
+    // So in order for this function to work:
+    //   1. Setup the custom asset.
+    //   2. Setup the list_resource.
+    //   3. Setup the list parent.
+    //   4. run the function.
     let shared_font = asset_server.load(PATH_SIMPLE_FONT);
     let parent_entity = query_parent.get_single().unwrap();
 
-    for race in V::vec().iter() {
+    for key in V::vec().iter() {
         if let Some(descr_text) = race_asset
             .iter()
-            .filter(|(_handle_id, asset)| asset.key() == *race)
+            .filter(|(_handle_id, asset)| asset.key() == *key)
             .map(|(_handle, asset)| asset.description())
             .next()
         {
@@ -213,7 +245,7 @@ pub fn build_description_list<T, U, V>(
                         // Alternate Racial Trait Title
                         TextBundle {
                             text: Text::from_section(
-                                race.to_string(),
+                                key.to_string(),
                                 TextStyle {
                                     font: shared_font.clone(),
                                     font_size: DESCRIPTION_FONT_SIZE,
